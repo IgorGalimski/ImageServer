6f67631cde9734f0c6db401027d8cd8331535139
IgorGalimski <igor.galimski@gmail.com>
2023-05-12 13:31:14 +0300

    Add import mysql

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
@@ -13,16 +15,26 @@ func main() {
 	r.POST("/upload", uploadImage)
 	r.GET("/", ping)
 
-	err := r.Run()
+	err := r.Run(":8081")
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
 func ping(c *gin.Context) {
+	getDB()
+
 	c.String(http.StatusOK, "Running!")
 }
 
 func uploadImage(c *gin.Context) {
 
+}
+
+func getDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/images")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return db
 }
