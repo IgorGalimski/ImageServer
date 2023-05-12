8f52d4527b39e61c9a846a799892e2cac57e1237
IgorGalimski <igor.galimski@gmail.com>
2023-05-12 13:41:45 +0300

    Add saving file to the drive

--- main.go
+++ main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -28,7 +32,54 @@ func ping(c *gin.Context) {
 }
 
 func uploadImage(c *gin.Context) {
+	file, err := c.FormFile("image")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Error getting image from user: %s", err.Error()))
+		return
+	}
 
+	fileName := fmt.Sprintf("%s%s", uuid.New().String(), filepath.Ext(file.Filename))
+	fullPathToFile := fmt.Sprintf("uploads/%s", fileName)
+
+	err = c.SaveUploadedFile(file, fullPathToFile)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Error saving file: %s", err.Error()))
+		return
+	}
+
+	f, err := os.Open(fileName)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Error opening file: %s", err.Error()))
+		return
+	}
+	defer f.Close()
+
+	userId, err := c.FormFile("userId")
+	if err != nil {
+		c.String(http.StatusBadRequest, fmt.Sprintf("Error getting user id: %s", err.Error()))
+		return
+	}
+
+	db := getDB()
+	defer db.Close()
+
+	stmt, err := db.Prepare("INSERT INTO images (fileName, userId) VALUES (?, ?)")
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Error: %s", err.Error()))
+		return
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Exec(fileName, userId)
+	if err != nil {
+		c.String(http.StatusInternalServerError, fmt.Sprintf("Error saving file to database: %s", err.Error()))
+
+		_ = os.Remove(fullPathToFile)
+
+		return
+	}
+
+	c.String(http.StatusOK, fmt.Sprintf("File %s uploaded successfully", file.Filename))
 }
 
 func getDB() *sql.DB {
